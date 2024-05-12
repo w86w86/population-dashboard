c478b221e6d38deeb566b928e7f2cfd52a8583aa
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-05-12 00:58:11 -0500

    Update streamlit_app_simple.py

--- streamlit_app_simple.py
+++ streamlit_app_simple.py
@@ -6,8 +6,7 @@ import altair as alt
 import plotly.express as px
 from censustools import *
 
-#######################
-# Page configuration
+#######################  # Page configuration
 st.set_page_config(
     page_title="US Population Census - Semester project 2",
     page_icon="🏂",
@@ -16,14 +15,11 @@ st.set_page_config(
 
 alt.themes.enable("dark")
 
-
-#######################
-# Load data
-
-
+#######################  # Load data
 api2=API('github')
 st.write('1*********************')
 st.write(api2.vars['columns']['citiz'])
 st.write('2*********************')
 
 api2.df = readCSV_and_fuse_dfs_04(api2)
+st.write(api2.df.head())
