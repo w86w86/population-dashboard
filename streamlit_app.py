4c8794b4b1aeb173841f6e37364989f5b2b107cd
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-05-12 00:10:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
-import censustools
+from censustools import *
 
 #######################
 # Page configuration
